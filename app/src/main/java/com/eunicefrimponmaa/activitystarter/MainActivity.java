42d0b7ec9c25050a45f5bd7ed29005eacf0dede9
frimponmaa <eeyfagyei@st.ug.edu.gh>
2018-06-09 02:42:06 +0300

    initial commit

---
+++ app/src/main/java/com/eunicefrimponmaa/activitystarter/MainActivity.java
@@ -0,0 +1,23 @@
+package com.eunicefrimponmaa.activitystarter;
+
+import android.content.Intent;
+import android.view.View;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void launchIntent(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.packtpub.com/"));
+        startActivity(intent);
+    }
+}
